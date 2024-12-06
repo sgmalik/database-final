c5873e6b1b9760a3df833dceed533ef49c491cac
sgmalik <surya.malik@uvm.edu>
2024-12-06 16:37:58 -0500

    trimmed db

--- database_setup.py
+++ database_setup.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 conn = sqlite3.connect('final_project.db')
 
-game_df = pd.read_csv('game.csv')
-player_df = pd.read_csv('common_player_info.csv')
-team_df = pd.read_csv('team.csv')
+game_df = pd.read_csv('game.csv').tail(500)
+player_df = pd.read_csv('common_player_info.csv').tail(500)
+team_df = pd.read_csv('team.csv').tail(500)
 
 game_df.to_sql('game', conn, if_exists='replace', index=False)
 player_df.to_sql('player', conn, if_exists='replace', index=False)
